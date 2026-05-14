bd56d16adc772d078d4aed88ac05d20ef3b1a9fb
cds-amal <cds.sudama@gmail.com>
2026-05-14 18:43:39 -0400

    3 - impl take

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -13,10 +13,15 @@ declare_id!("H1GjRKWSauAuupurDtGiY5uvhLBtUngNhvrSBs75rH9o");
 pub mod escrow {
     use super::*;
 
-    #[instruction(discriminator=0)]
-    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result <()> {
+    #[instruction(discriminator = 0)]
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
         ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
         ctx.accounts.deposit(deposit)
     }
 
+    #[instruction(discriminator = 1)]
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
 }
