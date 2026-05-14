7a2d885fabd7c08682a04ccc8cdc2e4e0829212f
cds-amal <cds.sudama@gmail.com>
2026-05-14 18:41:34 -0400

    1 - take

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -3,8 +3,6 @@ pub mod error;
 pub mod instructions;
 pub mod state;
 
-use anchor_lang::prelude::*;
-
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
@@ -15,7 +13,10 @@ declare_id!("H1GjRKWSauAuupurDtGiY5uvhLBtUngNhvrSBs75rH9o");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    #[instruction(discriminator=0)]
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result <()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
     }
+
 }
